e88a0127761330c10eb9b245807cb0ebc125fc87
Mike Smith <gooseisdead@mac.com>
2020-09-08 16:06:15 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -11,25 +11,44 @@ end
 
 
 def consolidate_cart(cart)
-  final_cart = []
-  counter = 0
-  while counter < cart.length
-    new_cart_item = find_item_by_name_in_collection(cart[counter][:item], final_cart)
-    if new_cart_item = !nil
-      new_cart_item[:count] += 1
+  def consolidate_cart(cart)
+  output = {}
+  cart.each do |item|
+    item_name = item.keys[0]
+    if output[item_name]
+      output[item_name][:count] += 1 
     else
-      new_cart_item = {
-        :item => cart[counter][:item],
-        :price => cart[counter][:price],
-        :clearance => cart[counter][:clearance],
-        :count => 1
-      }
-      final_cart << new_cart_item
+      output[item_name] = item[item_name]
+      output[item_name][:count] = 1 
     end
-    counter += 1
   end
-  final_cart
+  output
 end
+#   new_cart = []
+#   counter += 1
+#   while counter < cart.length
+# 
+#   end
+# end
+#   final_cart = []
+#   counter = 0
+#   while counter < cart.length
+#     new_cart_item = find_item_by_name_in_collection(cart[counter][:item], final_cart)
+#     if new_cart_item = !nil
+#       new_cart_item[:count] += 1
+#     else
+#       new_cart_item = {
+#         :item => cart[counter][:item],
+#         :price => cart[counter][:price],
+#         :clearance => cart[counter][:clearance],
+#         :count => 1
+#       }
+#       final_cart << new_cart_item
+#     end
+#     counter += 1
+#   end
+#   final_cart
+# end
   # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
