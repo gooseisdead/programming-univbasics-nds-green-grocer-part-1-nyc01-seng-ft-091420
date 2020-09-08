ef79851f4128224013a562b4d52a5e34f7e516f2
Mike Smith <gooseisdead@mac.com>
2020-09-08 16:08:20 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -10,16 +10,16 @@ end
 
 
 
-def consolidate_cart(cart) 
-  new_cart = {} 
-  cart.each do |items_array| 
-    items_array.each do |item, attribute_hash| 
-      new_cart[item] ||= attribute_hash 
-      new_cart[item][:count] ? new_cart[item][:count] += 1 :   
-      new_cart[item][:count] = 1 
-  end 
-end 
-new_cart 
+def consolidate_cart(cart)
+  new_cart = {}
+  cart.each do |items_array|
+    items_array.each do |item, attribute_hash|
+      new_cart[item] ||= attribute_hash
+      new_cart[item][:count] ? new_cart[item][:count] += 1 :
+      new_cart[item][:count] = 1
+  end
+end
+new_cart
 end
 #   new_cart = []
 #   counter += 1
